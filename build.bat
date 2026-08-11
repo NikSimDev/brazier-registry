ac39a6257024065a5561259da319d2be7b4cf9a1
CodeGonshik <nikolaysimson20@gmail.com>
2026-08-11 20:16:41 +0300

    for local usage final

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if defined VCPKG_ROOT (
+    set VCPKG_PATH=%VCPKG_ROOT%
+) else (
+    set VCPKG_PATH=C:\Tools\vcpkg
+)
+
+set PORTS_PATH=%~dp0ports
+
+rmdir /s /q "%VCPKG_PATH%\packages\lightlib_x64-windows" 2>nul
+rmdir /s /q "%VCPKG_PATH%\buildtrees\lightlib" 2>nul
+
+"%VCPKG_PATH%\vcpkg.exe" remove lightlib:x64-windows --purge
+
+"%VCPKG_PATH%\vcpkg.exe" install --overlay-ports="%PORTS_PATH%" lightlib --recurse --editable
--- ports/lightlib/portfile.cmake
+++ ports/lightlib/portfile.cmake
@@ -1,13 +1,11 @@
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO NikSimDev/lightlib
-    REF v0.1.4
-    SHA512 cde4708a579a0e9c5565c6db333f9c44d6a28ba4c36e1b26f2d63927e42df6067bd5068332338b43f34aa305a0c0cc7f9af74ef5a395959856da114dfaf9ce8e
-    HEAD_REF master
-)
+set(SOURCE_PATH "C:/Users/Admin/source/repos/Light")
+
+if(NOT EXISTS "${SOURCE_PATH}")
+    message(FATAL_ERROR "Source directory not found: ${SOURCE_PATH}")
+endif()
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}/Light"
+    SOURCE_PATH "${SOURCE_PATH}/Light"   
     OPTIONS
         -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
 )
